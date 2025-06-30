ed7654ed69fdde1a49056322e6b774a119c6f7ac
Joselias <joselias.dev@gmail.com>
2025-06-30 17:54:11 -0300

    add: database configurations

--- backend/src/main/java/com/nocountry/conversafe/Models/Dtos/Mensaje/MensajeRequestDTO.java
+++ backend/src/main/java/com/nocountry/conversafe/Models/Dtos/Mensaje/MensajeRequestDTO.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+
 public record MensajeRequestDTO(
         Usuario autor,
         String contenido) {
