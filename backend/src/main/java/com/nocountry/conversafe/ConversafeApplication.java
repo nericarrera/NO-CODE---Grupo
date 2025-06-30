d26fe6cea792b12cec0b9c9ff13234ccbceddec4
Joselias <joselias.dev@gmail.com>
2025-06-30 14:28:06 -0300

    first: commit

---
+++ backend/src/main/java/com/nocountry/conversafe/ConversafeApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.conversafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversafeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nocountry/conversafe/ConversafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.conversafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
